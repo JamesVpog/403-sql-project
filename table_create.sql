35523e18e822e0c1d8393f86ff121cf31be9eee7
tevesjl <teves@mines.edu>
2023-04-07 13:48:19 -0600

    Added statements to set owner for new college tables to the group

--- table_create.sql
+++ table_create.sql
@@ -59,7 +59,9 @@ CREATE TABLE space_missions(
 
 /* allow access to all tables for everyone in group*/
 
-ALTER TABLE s23_group48.astronaut OWNER TO s23_group48;
-ALTER TABLE s23_group48.college OWNER TO s23_group48;
-ALTER TABLE s23_group48.space_missions OWNER TO s23_group48;
+ALTER TABLE IF EXISTS s23_group48.astronaut OWNER TO s23_group48;
+ALTER TABLE IF EXISTS s23_group48.college OWNER TO s23_group48;
+ALTER TABLE IF EXISTS s23_group48.space_missions OWNER TO s23_group48;
+ALTER TABLE IF EXISTS s23_group48.college_employment_statistics OWNER TO s23_group48;
+ALTER TABLE IF EXISTS s23_group48.college_earnings OWNER TO s23_group48;
 
