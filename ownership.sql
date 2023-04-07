a661caa6c49623f4416f097543f5ae04a35bcbde
tevesjl <teves@mines.edu>
2023-04-07 13:53:09 -0600

    Added ownership.sql file to set ownership for all tables to the group

---
+++ ownership.sql
@@ -0,0 +1,6 @@
+/* allow access to all tables for everyone in group*/
+ALTER TABLE IF EXISTS s23_group48.astronaut OWNER TO s23_group48;
+ALTER TABLE IF EXISTS s23_group48.college OWNER TO s23_group48;
+ALTER TABLE IF EXISTS s23_group48.space_missions OWNER TO s23_group48;
+ALTER TABLE IF EXISTS s23_group48.college_employment_statistics OWNER TO s23_group48;
+ALTER TABLE IF EXISTS s23_group48.college_earnings OWNER TO s23_group48;
--- table_create.sql
+++ table_create.sql
@@ -56,12 +56,3 @@ CREATE TABLE space_missions(
     price TEXT, 
     mission_status TEXT
 );
-
-/* allow access to all tables for everyone in group*/
-
-ALTER TABLE IF EXISTS s23_group48.astronaut OWNER TO s23_group48;
-ALTER TABLE IF EXISTS s23_group48.college OWNER TO s23_group48;
-ALTER TABLE IF EXISTS s23_group48.space_missions OWNER TO s23_group48;
-ALTER TABLE IF EXISTS s23_group48.college_employment_statistics OWNER TO s23_group48;
-ALTER TABLE IF EXISTS s23_group48.college_earnings OWNER TO s23_group48;
-
