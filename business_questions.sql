8394603050b4188c5f242491e22139d513c44200
derek-d-wang <derekw2012@gmail.com>
2023-04-19 15:06:35 -0600

    Added a business question

--- business_questions.sql
+++ business_questions.sql
@@ -7,3 +7,10 @@
 SELECT major, ugrad_major, name, e.unemployement_rate
 FROM college_employment_statistics AS e 
     JOIN astronaut_v2 AS a ON lower(e.major) = lower(a.ugrad_major);
+
+-- Average age on each mission
+SELECT a.mission, m.launch_date, 
+AVG (DATE_PART('year', m.launch_date)-DATE_PART('YEAR', A.birth_date))AS avg_age
+FROM astronaut_v2 AS a JOIN mission_info AS m ON a.mission = m.mission_name
+GROUP BY a.mission, m.launch_date
+ORDER BY m.launch_date;
