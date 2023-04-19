ab75ace3305a89340d3ae63f018e57a42d5a3eb6
James Vongphasouk <james.vongpha@gmail.com>
2023-04-18 21:19:49 -0600

    ran because i owner

--- ownership.sql
+++ ownership.sql
@@ -1,8 +1,8 @@
 /* allow access to all tables for everyone in group*/
-ALTER TABLE IF EXISTS s23_group48.astronaut OWNER TO s23_group48;
+ALTER TABLE IF EXISTS s23_group48.astronaut OWNER TO s23_group48; --skips anyways
 ALTER TABLE IF EXISTS s23_group48.astronaut_v2 OWNER TO s23_group48;
 ALTER TABLE IF EXISTS s23_group48.college OWNER TO s23_group48; --skips anyways
-ALTER TABLE IF EXISTS s23_group48.space_missions OWNER TO s23_group48;
+ALTER TABLE IF EXISTS s23_group48.space_missions OWNER TO s23_group48; --skips anyways
 ALTER TABLE IF EXISTS s23_group48.college_employment_statistics OWNER TO s23_group48;
 ALTER TABLE IF EXISTS s23_group48.college_earnings OWNER TO s23_group48;
 ALTER TABLE IF EXISTS s23_group48.rocket_info OWNER TO s23_group48;
