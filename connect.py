68ced0fc12c32c1ef9babfebd8bbbd2117ca9ea7
James Vongphasouk <james.vongpha@gmail.com>
2023-04-20 21:44:35 -0600

    added things

--- connect.py
+++ connect.py
@@ -5,25 +5,15 @@ matplotlib.use('Agg')
 
 password = getpass.getpass('Password: ')
 
-conn = psycopg2.connect(
-    host="codd.mines.edu",
-    database="csci403",
-    user="jvongphasouk",
-    port=5433,
-    password= password)
-		
-
+conn = psycopg2.connect( host="codd.mines.edu", database="csci403", user="jvongphasouk", port=5433, password= password)
 cur = conn.cursor()
 
 cur.execute("SET search_path TO s23_group48")
-
 query = """
 SELECT name, DATE_PART('year', launch_date) - DATE_PART('year', birth_date) AS age, mission
 FROM 
     astronaut_v2 AS a 
     JOIN mission_info AS s ON a.mission = s.mission_name; """
-         
-
 cur.execute(query)
 
 resultset = cur.fetchall()
@@ -33,13 +23,10 @@ for row in resultset:
     name, age, mission = row
     ages.append(age)
 
-#data science
-
 import matplotlib.pyplot as plt
 
-plt.hist(ages)
+plt.hist(ages) #get age histogram
 plt.title('Age of Astronauts at time of Launch')
 plt.xlabel('Age')
 plt.savefig('ages.png')
-
 
